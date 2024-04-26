2af1eb0728b51400fcb6cfde6878d49ffddc42ac
Ezeibekwe Emmanuel <ezeibekweemma@gmail.com>
2024-04-26 20:29:57 +0100

    Update object detection threshold in CameraDetection component

--- components/CameraDetection.tsx
+++ components/CameraDetection.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { renderPredictions } from '@/utils/render-predictions';
 import { load, ObjectDetection } from '@tensorflow-models/coco-ssd';
 import * as tf from '@tensorflow/tfjs';
 import { useEffect, useRef, useState } from 'react';
@@ -42,31 +43,19 @@ function CameraDetection() {
         const detectedObjects = await model.detect(
           (webcamRef.current as any).video,
           undefined,
-          0.6 // A threshold of 0.6 is used to determine the confidence of the detected objects.
+          0.4 // A threshold of 0.6 is used to determine the confidence of the detected objects.
         );
-        console.log(detectedObjects);
+
         const context = (canvasRef.current as any).getContext('2d');
-        // renderPredictions(detectedObjects, context);
+        renderPredictions(detectedObjects, context);
       } catch (error) {
         console.error('Error detecting objects:', error);
       }
     }
   }
 
-  // const handleLoadedMetadata = () => {
-  //   if (webcamRef.current && webcamRef.current !== null &&
-  // webcamRef.current.video?.readyState === 4) {
-  //     const myVideoWidth = (webcamRef.current as any).video.videoWidth;
-  //     const myVideoHeight = (webcamRef.current as any).video.videoHeight;
-
-  //     (webcamRef.current as any).video.width = myVideoWidth;
-  //     (webcamRef.current as any).video.height = myVideoHeight;
-  //   }
-  // };
-
   useEffect(() => {
     startCoco();
-    // showmyVideo();
     return () => {
       clearInterval(detectInterval);
     };
@@ -77,10 +66,8 @@ function CameraDetection() {
       <Webcam
         ref={webcamRef}
         audio={false}
-        mirrored={true}
         muted
         className="h-full w-full border-2 border-primary"
-        // onLoadedMetadata={handleLoadedMetadata}
       />
       <canvas
         className="absolute top-0 left-0 z-50 h-full w-full"
---
+++ utils/render-predictions.ts
@@ -0,0 +1,20 @@
+export const renderPredictions = (predictions: any[], ctx: any) => {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+  // Fonts
+  // const font = '12px';
+  ctx.textBaseline = 'bottom';
+
+  predictions.forEach((prediction) => {
+    const [x, y, width, height] = prediction['bbox'];
+    const result = `${prediction.class}: ${(prediction.score * 100).toFixed(
+      1
+    )}%`;
+
+    // bounding box
+    ctx.strokeStyle = ctx.fillStyle = '#22C55E';
+    ctx.lineWidth = 1;
+    ctx.strokeRect(x, y, width, height);
+    ctx.fillText(result, x - 1, y - 2);
+  });
+};
